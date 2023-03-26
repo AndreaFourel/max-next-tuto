077e0b604b3d1c41e9337b39df0b2ffc7db035c3
Andrea Fourel <andreafourel@hotmail.fr>
2023-03-26 13:01:18 +0200

    extracting data for filteredEvents page & styled error pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,7 @@ import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function EventDetailPage () {
 
@@ -13,7 +14,13 @@ function EventDetailPage () {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No event found</p>
+    return(
+      <Fragment>
+        <ErrorAlert>
+          <p>No event found</p>
+        </ErrorAlert>
+      </Fragment>
+    );
   } else {
     return (
       <Fragment>
